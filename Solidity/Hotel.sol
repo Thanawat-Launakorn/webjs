68aedc015bbb24b261e1f15ad014e61ab3191e53
Thanawat <everlastbw@hotmail.com>
2022-03-06 04:06:28 +0700

    finish

--- Solidity/Hotel.sol
+++ Solidity/Hotel.sol
@@ -1,53 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract ProofOfStudent {  
+contract bangsaenVille {  
 
- struct Tx {
-        string time;
-        string room;
-        address owner;
-        bool status;
-    }
-  mapping (string => Tx) private ListRooms;
+  mapping (bytes32 => bool) private listReserve;
 
   //---events---
-  event reserve(
-    address from,   
-    Tx text
-  );
-
-  event reserveError(
-    address from,   
-    string text
+  event reserveAdded(
+    address from,
+    string timestamp,   
+    string hotelname,
+    bytes32 hash
   );
   
+  event reserveError(
+    address from,
+    string text,
+    string reason
+  );
+
+  // store the proof for a student in the contract state
+  function recordReserve(bytes32 proof) private {
+    listReserve[proof] = true;
+  }
   
   // record a student name
-  function reserveRoom(string memory time,string memory room) public payable {
+  function reserve(string memory t,string memory h) public payable {
     
-    //---check if string was previously stored---
-    if (ListRooms[room].status) {
-        //---fire the event---
-        emit reserveError(msg.sender, "Room not empty");
-        //---refund back to the sender---
+    if (msg.value < 0.001 ether) {
+        emit reserveError(msg.sender, h, "Please pay value >= 0.001 eth");
         payable(msg.sender).transfer(msg.value);
-        //---exit the function---
         return;
     }
- // store the proof for a Rooms in the contract state
+    recordReserve(hashing(t));
+    emit reserveAdded(msg.sender, t, h, 
+        hashing(t));
     
-    ListRooms[room] = Tx(time,room,msg.sender,true);
-    
-    
-    //---fire the event---
-    emit reserve(msg.sender, ListRooms[room]);
-   
   }
+  
+  // SHA256 for Integrity
+  function hashing(string memory name) private 
+  pure returns (bytes32) {
+    return sha256(bytes(name));
+  }
+  
   // check name of student in this class
-  function checkName(string memory name) public 
-  view returns (Tx memory) {
-    return ListRooms[name];
+  function checkReserve(string memory name) public 
+  view returns (bool) {
+    return listReserve[hashing(name)];
   }
-
 }
